4cd725e44eca765804068c513f6a1706ebacc23c
Sơn Ngọc <sondnpt00343@gmail.com>
2020-03-08 08:22:31 +0700

    run prettier locally

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: false,
-      exports: 'named'
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: false
-    }
-  ],
-  plugins: [
-    external(),
-    url(),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
-    }),
-    resolve(),
-    commonjs()
-  ],
-  external: [ 'socketcluster-client' ]
-}
+    input: 'src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: false,
+            exports: 'named'
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: false
+        }
+    ],
+    plugins: [
+        external(),
+        url(),
+        svgr(),
+        babel({
+            exclude: 'node_modules/**',
+            plugins: ['external-helpers']
+        }),
+        resolve(),
+        commonjs()
+    ],
+    external: ['socketcluster-client']
+};
--- src/index.js
+++ src/index.js
@@ -1,81 +1,81 @@
 let settings = {
-  ignoreFocused: true,
-  timeout: 0,
-  cooldown: 0
+    ignoreFocused: true,
+    timeout: 0,
+    cooldown: 0
 };
 let available = false;
 let cooldownActive = false;
 let focused = true;
 
 function initNotifications(options) {
-  // Handle old browsers - this way we can skip polyfill Promise and Object.assign
-  if (!window.hasOwnProperty('Notification')) {
-    console.info('This browser does not support notifications');
+    // Handle old browsers - this way we can skip polyfill Promise and Object.assign
+    if (!window.hasOwnProperty('Notification')) {
+        console.info('This browser does not support notifications');
 
-    // Mock promise always resolving to false
-    return {
-      then: fn => fn(false)
-    };
-  }
-
-  settings = Object.assign({}, settings, options);
-
-  window.onfocus = () => {
-    focused = true;
-  };
-
-  window.onblur = () => {
-    focused = false;
-  };
-
-  const availablePromise = new Promise((resolve, reject) => {
-    if (Notification.permission === 'granted') {
-      resolve(true);
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-          resolve(true);
-        }
-        resolve(false);
-      });
-    } else {
-      resolve(false);
+        // Mock promise always resolving to false
+        return {
+            then: fn => fn(false)
+        };
     }
-  });
 
-  availablePromise.then(result => {
-    available = result;
-  });
+    settings = Object.assign({}, settings, options);
 
-  return availablePromise;
+    window.onfocus = () => {
+        focused = true;
+    };
+
+    window.onblur = () => {
+        focused = false;
+    };
+
+    const availablePromise = new Promise((resolve, reject) => {
+        if (Notification.permission === 'granted') {
+            resolve(true);
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    resolve(true);
+                }
+                resolve(false);
+            });
+        } else {
+            resolve(false);
+        }
+    });
+
+    availablePromise.then(result => {
+        available = result;
+    });
+
+    return availablePromise;
 }
 
 function notify(title, notifyOptions) {
-  if (!available || (settings.ignoreFocused && focused) || cooldownActive) {
-    return null;
-  }
+    if (!available || (settings.ignoreFocused && focused) || cooldownActive) {
+        return null;
+    }
 
-  const notification = new Notification(title, notifyOptions);
+    const notification = new Notification(title, notifyOptions);
 
-  notification.onclick = () => {
-    window.focus();
-    notification.close();
-  };
+    notification.onclick = () => {
+        window.focus();
+        notification.close();
+    };
 
-  if (settings.timeout !== 0) {
-    window.setTimeout(() => {
-      notification.close();
-    }, settings.timeout);
-  }
+    if (settings.timeout !== 0) {
+        window.setTimeout(() => {
+            notification.close();
+        }, settings.timeout);
+    }
 
-  if (settings.cooldown !== 0) {
-    cooldownActive = true;
-    window.setTimeout(() => {
-      cooldownActive = false;
-    }, settings.cooldown);
-  }
+    if (settings.cooldown !== 0) {
+        cooldownActive = true;
+        window.setTimeout(() => {
+            cooldownActive = false;
+        }, settings.cooldown);
+    }
 
-  return notification;
+    return notification;
 }
 
-export {initNotifications, notify};
+export { initNotifications, notify };
